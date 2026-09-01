cf29e9046007fb93e4003d1bba43ba088a6495ee
DevPedrin <devhenrik09@gmail.com>
2026-09-01 18:26:02 -0300

    Melhorias e correção de bugs

--- aulas/desafio-aula05/src/programa/Biblioteca.java
+++ aulas/desafio-aula05/src/programa/Biblioteca.java
@@ -11,10 +11,12 @@ import java.util.List;
 public class Biblioteca {
     private String nome; 
     private List<Livro> livros;
-    public Biblioteca() {};
+    public Biblioteca() {
+        this.livros = new ArrayList<>();
+    };
 
     public Biblioteca(String nome, List<Livro> livros) {
-        this.livros = livros;
+        this.livros = (livros != null) ? livros : new ArrayList<>();
         this.nome = nome;
     } 
 
@@ -22,34 +24,78 @@ public class Biblioteca {
         return nome;
     }
     
-
     public List<Livro> getLivros() {
         return livros;
     }
+
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
     public void setLivros(List<Livro> livros) {
-        this.livros = livros;
+        if(!livros.isEmpty()) {
+            this.livros = livros;
+        }
     }
 
     public void addLivro(Livro livro) {
-        this.livros.add(livro);
+        this.livros.add(livro);    
     }
 
     public void removerLivro(Livro livro) {
-        this.livros.remove(livro);
+        if(!livros.isEmpty()) {
+            this.livros.remove(livro);
+        }
+    }
+
+    // extra, so para padronizar e não repetir nos outros metados
+    private void imprimirLivro(Livro livro) {
+        System.out.printf("""
+            Titulo: %s%n
+            Autor: %s%n
+            Editora: %s%n
+            Preço: %.2f%n
+            Quantidade de páginas: %d%n
+            ----------------------------
+
+            """,
+            livro.getTitulo(),
+            livro.getAutor(),
+            livro.getEditora(),
+            livro.getPreco(),
+            livro.getQtdPaginas()
+        );
+    }
+
+    // extra
+    public void imprimirTodosLivros()  {
+        for(Livro livro : livros) {
+            imprimirLivro(livro);
+        }
+    }
+
+    // extra
+    public void imprimirLivrosAutor(String nomeAutor) {
+        for(Livro livro : livros) { 
+            if(livro.getAutor().equals(nomeAutor)) {
+                imprimirLivro(livro);
+            }
+        }
     }
 
     public void imprimirLivroMaisCaro() {
-        System.out.print(Collections.max(livros, Comparator.comparingDouble(Livro::getPreco)));  //  mais simples
+        if(!livros.isEmpty()) {
+            Livro livroMaisCaro = Collections.max(livros, Comparator.comparingDouble(Livro::getPreco));  //mais simples
+
+            imprimirLivro(livroMaisCaro);
+
+        }
 
         // sim eu sei fazer a implementação de percorrer a estrutura de dados e comparar, so to testando coisas uteis do java
         // tbm poderia fazer com contador no for etc...
         /* 
-        Livro maisCaro = livros.get(0); 
+        Livro livroMaisCaro = livros.get(0); 
 
         for(Livro l : livros) {
             if(l.getPreco() >= maisCaro.getPreco()) {
@@ -61,44 +107,71 @@ public class Biblioteca {
         */
     }
 
-    // extra, e só o inverso e tals 
+    // extra, mostra o mais barato  
     public void imprimirLivroMaisBarato() {
-        System.out.println(Collections.min(livros, Comparator.comparing(Livro::getPreco)));
+        if(!livros.isEmpty()) {
+            Livro livroMaisBarato = Collections.min(livros, Comparator.comparingDouble(Livro::getPreco));  //mais simples
+
+            imprimirLivro(livroMaisBarato);
+        }
+    }
+
+    // extra
+    public int qtdTotalLivros() {
+        return livros.size();
+    }
+
+    // extra, retorna o preço total de todos os livros
+    public double precoTotalLivros() {
+        double sum = 0;
+        
+        if(livros.isEmpty()) {
+            return 0;
+        } 
+
+        for(Livro livro : livros) {
+            sum += livro.getPreco();
+        }
+
+        return sum;
     }
 
     public double calcularMedia() {
-        double sum = 0;
+        if(livros.isEmpty()) {
+            return 0;
+        } 
 
-        // o tal do foreach
-        for(Livro l : livros) {
-            sum += l.getPreco();
-        }
-
-        return sum / livros.size();
+        return precoTotalLivros() / qtdTotalLivros(); // usa os metados da classe para calcular a media
     }
 
     public int contarLivrosAutor(String nomeAutor) {
         int qtd = 0;
-        
+
         for(Livro livro : livros) {
             if(livro.getAutor().equals(nomeAutor)) {
-                qtd += 1;
+                qtd++;
             }
         }
-
+        
         return qtd;
     }  
-    
-    // extra
+
+    // extra, lista todos os livros filtrando pelo autor
     public List<Livro> listaLivrosAutor(String nomeAutor) {
+        // poderia ter um try/catch para ver se existe esse autor
         List<Livro> livrosAutor = new ArrayList<>();
 
-        for(Livro livro : livros) {
+        for(Livro livro : livros) { 
             if(livro.getAutor().equals(nomeAutor)) {
                 livrosAutor.add(livro);
             }
         }
 
         return livrosAutor;
+    }
+
+    @Override
+    public String toString() {
+        return "Biblioteca [nome=" + nome + ", totalLivros=" + qtdTotalLivros() + ", precoTotal=" + precoTotalLivros() + "]";
     }
 }
--- aulas/desafio-aula05/src/programa/Main.java
+++ aulas/desafio-aula05/src/programa/Main.java
@@ -3,72 +3,41 @@
 package  programa;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Lista de livros para "adicionar" na biblioteca 
-        List<Livro> livros = new ArrayList<>();
+        Livro livro1 = new Livro("O Pequeno Príncipe", "Antonie de Saint-Exupéry", "Editora Lafonte", 74.93, 96);
+        Livro livro2 = new Livro("Use a cabeça! HTML e CSS", "Elisabeth Robson", "Alta Books", 230, 496);
+        Livro livro3 = new Livro("Design Patterns", "Erich Gamma", "Addison-Wesley Professional", 133.94, 416);
+        Livro livro4 = new Livro("Clean Code", "Robert C. Martin", "Prentice Hall PTR", 198.05, 431);
+        Livro livro5 = new Livro("Use a cabeça! Java", "Bert Bates", "Alta Books", 60, 496);
+        Livro livro6 = new Livro("It: A Coisa", "Stephen King", "Suma", 100.00, 1104);
+        Livro livro7 = new Livro("O Iluminado", "Stephen King", "Suma", 80.00, 464);
+        
+        Biblioteca library = new Biblioteca("Minha Biblioteca", new ArrayList<>());
 
-        livros.add(
-            new Livro("O Pequeno Príncipe", "Antonie de Saint-Exupéry", "Editora Lafonte", 74.93, 96)
-        );
+        library.addLivro(livro1);
+        library.addLivro(livro2);
+        library.addLivro(livro3);
+        library.addLivro(livro4);
+        library.addLivro(livro5);
+        library.addLivro(livro6);
+        library.addLivro(livro7);
 
-        livros.add(
-            new Livro("Use a cabeça! HTML e CSS", "Elisabeth Robson", "Alta Books", 230, 496)
-        );
-
-        livros.add(
-            new Livro("Design Patterns", "Erich Gamma", "Addison-Wesley Professional", 133.94, 416)
-        );
-
-        livros.add(
-            new Livro("Clean Code", "Robert C. Martin", "Prentice Hall PTR", 198.05, 431)
-        );
-
-        livros.add(
-            new Livro("Use a cabeça! Java", "Bert Bates", "Alta Books", 60, 496)
-        );
-
-        livros.add(
-            new Livro("It: A Coisa", "Stephen King", "Suma", 100.00, 1104)
-        );
-
-        livros.add(
-            new Livro("O Iluminado", "Stephen King", "Suma", 80.00, 464)
-        );
-
-
-
-        Biblioteca library = new Biblioteca("lib1", livros);
-
-
-        System.out.printf("Quantidade de livros do Stephen King: %d", library.contarLivrosAutor("Stephen King"));
 
         /* 
-        //Lista de livros da Biblioteca
-        List<Livro> listaLivros = library.getLivros();
-        q
-        for(Livro livro : listaLivros) {
-            System.out.printf("""
-                Titulo: %s%n
-                Autor: %s%n
-                Editora: %s%n
-                Preço: %.2f%n
-                Quantidade de páginas: %d%n
-                ----------------------------
+        library.imprimirTodosLivros();
+        
+        library.removerLivro(livro2); 
+        
+        library.imprimirTodosLivros();
 
-                """,
-                livro.getTitulo(),
-                livro.getAutor(),
-                livro.getEditora(),
-                livro.getPreco(),
-                livro.getQtdPaginas()
-            );
-        }
+        
         */
 
+        //library.imprimirLivroMaisBarato();
         
+        library.calcularMedia();
+        library.contarLivrosAutor("Stephen King");
     }
-        
 }
